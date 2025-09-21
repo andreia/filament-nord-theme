765a0931d8ba8c51f1da618b0cd3111414a4bc91
Andréia Bohner <andreiabohner@gmail.com>
2025-09-21 14:52:13 -0300

    Merge pull request #27 from andreia/fix_styles_applied_to_another_panel
    Fix gray colors applied to another panels

--- src/FilamentNordThemePlugin.php
+++ src/FilamentNordThemePlugin.php
@@ -5,7 +5,6 @@ namespace Andreia\FilamentNordTheme;
 use Filament\Contracts\Plugin;
 use Filament\Panel;
 use Filament\Support\Colors\Color;
-use Filament\Support\Facades\FilamentColor;
 
 class FilamentNordThemePlugin implements Plugin
 {
@@ -21,25 +20,22 @@ class FilamentNordThemePlugin implements Plugin
 
     public function register(Panel $panel): void
     {
-        FilamentColor::register([
-            'gray' => [
-                50 => '#eceff4',  // nord6 - snow storm
-                100 => '#e5e9f0', // nord5 - snow storm
-                200 => '#d8dee9', // nord4 - snow storm
-                300 => '#a7b1c5',
-                400 => '#8c9ab3',
-                500 => '#71829b',
-                600 => '#4c566a', // nord3 - polar night
-                700 => '#434c5e', // nord2 - polar night
-                800 => '#3b4252', // nord1 - polar night
-                900 => '#2e3440', // nord0 - polar night
-                950 => '#232831',
-            ],
-        ]);
-
         $panel
             ->colors([
                 'danger' => Color::hex('#bf616a'), // nord11
+                'gray' => [
+                    50 => '#eceff4',  // nord6 - snow storm
+                    100 => '#e5e9f0', // nord5 - snow storm
+                    200 => '#d8dee9', // nord4 - snow storm
+                    300 => '#a7b1c5',
+                    400 => '#8c9ab3',
+                    500 => '#71829b',
+                    600 => '#4c566a', // nord3 - polar night
+                    700 => '#434c5e', // nord2 - polar night
+                    800 => '#3b4252', // nord1 - polar night
+                    900 => '#2e3440', // nord0 - polar night
+                    950 => '#232831',
+                ],
                 'info' => Color::hex('#81a1c1'), // nord9
                 'primary' => Color::hex('#88c0d0'), // nord8
                 'secondary' => Color::hex('#5e81ac'), // nord10
